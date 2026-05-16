d62dc62ea15fc32a48bc362d7e00624e6112b2d0
Jalalvba <chafiq.jalal@gmail.com>
2026-05-16 23:04:28 +0100

    fix: add boot delay for LCD init

--- lcd_sketch/lcd_sketch.ino
+++ lcd_sketch/lcd_sketch.ino
@@ -2,13 +2,10 @@
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 void setup() {
-  Serial.begin(9600);
-  Serial.println("setup started");
+  delay(100);
   lcd.init();
-  Serial.println("lcd init done");
   lcd.backlight();
   lcd.setCursor(0, 0);
   lcd.print("Hello Jalal");
-  Serial.println("print done");
 }
 void loop() {}
